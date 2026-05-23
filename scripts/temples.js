768e73b450884f88cfddfb245f5d313c864ba53a
bongan1988 <bsibanda3@byupathway.edu>
2026-05-23 16:31:22 +0200

    ij

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,15 @@
-document.getElementById("current-year").textContent = new Date().getFullYear();
-document.getElementById("modification-date").textContent = document.lastModified;
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+
+  // toggle icon
+  menuBtn.textContent = navMenu.classList.contains("open") ? "✖" : "☰";
+});
+
+// footer year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// last modified
+document.getElementById("lastModified").textContent = document.lastModified;
