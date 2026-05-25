1c7ded2178813e202b74393797cc9842efc68b17
bongan1988 <bsibanda3@byupathway.edu>
2026-05-25 10:47:38 +0200

    final js

---
+++ scripts/place.js
@@ -0,0 +1,4 @@
+// Current Year
+document.getElementById("year").textContent = new Date().getFullYear();
+//last modified
+document.getElementById("lastmodified").textContent = `Last Modified: ${document.lastModified}`;
